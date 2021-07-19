9a9d232b08c242a2085bca2c6e9acb29beb64ed0
kobayashiryou <chiri.parihead@gmail.com>
2021-07-19 16:32:59 +0900

    execution rubocop -a

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,10 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Question, type: :model do
   describe "validation check" do
-    subject{ question.valid? }
-    let(:question){ build(:question, body: body) }
-    let(:body){ Faker::Lorem.sentence }
+    subject { question.valid? }
+
+    let(:question) { build(:question, body: body) }
+    let(:body) { Faker::Lorem.sentence }
     context "bodyが指定されている時" do
       it "questionは作成される" do
         expect(subject).to eq true
@@ -12,7 +13,7 @@ RSpec.describe Question, type: :model do
     end
 
     context "bodyがnilの時" do
-      let(:body){ nil }
+      let(:body) { nil }
       it "エラーする" do
         subject
         expect(question.errors.messages[:body]).to include "can't be blank"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
   describe "validation check" do
-    subject{ user.valid? }
-    let(:user){ build(:user, uid: uid) }
-    let(:uid){ Faker::Alphanumeric }
+    subject { user.valid? }
+
+    let(:user) { build(:user, uid: uid) }
+    let(:uid) { Faker::Alphanumeric }
     context "uidが指定されている時" do
       it "userは作成される" do
         expect(subject).to eq true
       end
     end
+
     context "uidが指定されていない時" do
-      let(:uid){ nil }
+      let(:uid) { nil }
       it "エラーする" do
         subject
         expect(user.errors.messages[:uid]).to include "can't be blank"
